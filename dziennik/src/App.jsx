658252460154e804d889340ed1d6c226e632b44f
MrKermit <olafisz.pl@gmail.com>
2024-09-10 19:14:58 +0200

    nowe routy

--- dziennik/src/App.jsx
+++ dziennik/src/App.jsx
@@ -3,15 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Glowna from './components/kontentGlowna'
-import DodajSzkole from './components/Dyrekcja/dodajSzkole'
+import Glowna from './components/KontentGlowna'
+import Login from './components/Login.jsx'
+import AdminPage from './components/admin/AdminPage.jsx'
+import DodajUsera from './components/admin/DodajUsera.jsx'
+import DodajKlase from './components/admin/DodajKlase.jsx'
 
 function App() {
   return (
     <>
         <Routes>
           <Route index element={<Glowna/>} />
-          <Route element={<DodajSzkole/>} path='/dodajSzkole' />
+          <Route element={<Login/>} path='/login' />
+          <Route element={<AdminPage/>} path='/admin'/>
+          <Route element={<DodajUsera/>} path='/dodajUsera'/>
+          <Route element={<DodajKlase/>} path="/dodajKlase"/>
         </Routes>
     </>
   )
